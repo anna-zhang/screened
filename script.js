80f5393ae1fe14a75fce80a1e9c75d1477283579
Anna Zhang <31214438+anna-zhang@users.noreply.github.com>
2024-11-30 18:22:54 -0800

    basic button styling

--- script.js
+++ script.js
@@ -425,7 +425,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('mouseup', () => {
     if (isDragging) {
       isDragging = false
-      draggableElement.style.cursor = 'grab'
+      draggableControlsElement.style.cursor = 'grab'
     }
   })
 
